ab0737fd856e8bff6f67f0ec8cf652b78435f97f
Veilics <>
2023-06-07 18:57:58 +0800

    mongoDB working with tRPC

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,17 @@
 import { type NextPage } from "next";
+import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
+  const allShifts = api.getShifts.getAll.useQuery();
+
   return (
     <div>
       <div>
         <h1>Hello World</h1>
       </div>
+      <p>
+        {allShifts.data ? JSON.stringify(allShifts.data) : "Loading tRPC..."}
+      </p>
     </div>
   );
 };
