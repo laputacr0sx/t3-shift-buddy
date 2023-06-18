d52ee7d723df21fce67a483b7fa5af53be316d5c
Veilics <laputafelix@gmail.com>
2023-06-19 02:45:57 +0800

    fixing prisma error while mutating

--- src/pages/feature/index.tsx
+++ src/pages/feature/index.tsx
@@ -1,18 +1,22 @@
 import React from "react";
-import { api } from "~/utils/api";
+import { api, RouterOutputs } from "~/utils/api";
 
 export default function index() {
-  const { mutate, error, isLoading, isSuccess } =
-    api.updateShifts.createNextWeekPrefix.useMutation();
+  const { mutate: updatePrefixes } =
+    api.updateShifts.createNextWeekPrefix.useMutation({
+      onError(error) {
+        console.log(error.message);
+      },
+    });
 
-  if (isSuccess) console.log("done!");
+  type Prefix = RouterOutputs["updateShifts"]["createNextWeekPrefix"];
 
   return (
-    <div className="flex justify-center align-middle">
+    <div className="flex items-center justify-center">
       <button
-        className={"rounded-sm border-2 border-red-800"}
+        className={"border-2 border-red-800 text-blue-700"}
         onClick={() => {
-          mutate({
+          updatePrefixes({
             prefixes: ["D15", "D15", "B14", "D15", "D15", "A75", "Z71"],
           });
         }}
--- src/server/api/routers/updateShifts.ts
+++ src/server/api/routers/updateShifts.ts
@@ -4,11 +4,7 @@ import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 export const updateShiftRouter = createTRPCRouter({
   createNextWeekPrefix: publicProcedure
     .input(z.object({ prefixes: z.string().array().max(7) }))
-    .mutation(async ({ input, ctx }) => {
-      try {
-        await ctx.prisma.weekPrefix.create({ data: input.prefixes });
-      } catch (error) {
-        console.log(error);
-      }
+    .mutation(({ input, ctx }) => {
+      return ctx.prisma.weekPrefix.create({ data: input.prefixes });
     }),
 });
