5bc134a29b71a7cfbacf1c198f09e9e752a5ef70
Veilics <laputafelix@gmail.com>
2023-06-27 15:34:26 +0800

    fixing [AddToCalendarButton] render issue by implementing useEffect

--- src/components/AddShiftEventToCalendar.tsx
+++ src/components/AddShiftEventToCalendar.tsx
@@ -1,19 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
 import { AddToCalendarButton } from "add-to-calendar-button-react";
 
 function AddShiftEventToCalendar() {
+  const [hydrated, setHydrated] = useState(false);
+  useEffect(() => {
+    setHydrated(true);
+  }, []);
+
   return (
     <>
-      <AddToCalendarButton
-        name="Title"
-        options="'Apple','Google'"
-        location="World Wide Web"
-        startDate="2023-06-30"
-        endDate="2023-06-30"
-        startTime="10:15"
-        endTime="23:30"
-        timeZone="America/Los_Angeles"
-      ></AddToCalendarButton>
+      {hydrated && (
+        <AddToCalendarButton
+          name="Title"
+          options={["Apple", "Google", "Microsoft365"]}
+          location="World Wide Web"
+          startDate="2023-06-30"
+          endDate="2023-06-30"
+          startTime="10:15"
+          endTime="23:30"
+          timeZone="America/Los_Angeles"
+        ></AddToCalendarButton>
+      )}
     </>
   );
 }
--- src/pages/feature/index.tsx
+++ src/pages/feature/index.tsx
@@ -1,9 +1,23 @@
 import React from "react";
 
+import AddShiftEventToCalendar from "~/components/AddShiftEventToCalendar";
+
+const eventData = {
+  name: "Title",
+  options: ["Apple", "Google"],
+  location: "World Wide Web",
+  startDate: "2023-06-30",
+  endDate: "2023-06-30",
+  startTime: "10:15",
+  endTime: "23:30",
+  timeZone: "America/Los_Angeles",
+};
+
 function index() {
   return (
     <div>
       <h1>This is the calendar feature</h1>
+      <AddShiftEventToCalendar />
     </div>
   );
 }
