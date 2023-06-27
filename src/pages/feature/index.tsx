7ce6aa63efc05c29b1be91f9b353873a96c09874
Veilics <laputafelix@gmail.com>
2023-06-27 16:13:27 +0800

    fixing code

--- src/pages/feature/index.tsx
+++ src/pages/feature/index.tsx
@@ -2,17 +2,6 @@ import React from "react";
 
 import AddShiftEventToCalendar from "~/components/AddShiftEventToCalendar";
 
-const eventData = {
-  name: "Title",
-  options: ["Apple", "Google"],
-  location: "World Wide Web",
-  startDate: "2023-06-30",
-  endDate: "2023-06-30",
-  startTime: "10:15",
-  endTime: "23:30",
-  timeZone: "America/Los_Angeles",
-};
-
 function index() {
   return (
     <div>
