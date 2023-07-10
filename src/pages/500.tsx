0cdec29ec36b6bf40a7e196de1792d5d68c90447
Veilics <laputafelix@gmail.com>
2023-07-10 17:59:32 +0800

    added 500 error page

---
+++ src/pages/500.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import Link from "next/link";
+
+export default function Custom500() {
+  return (
+    <main className="flex h-screen w-full flex-col items-center justify-center bg-sky-950">
+      <h1 className="text-9xl font-extrabold tracking-widest text-slate-200">
+        500
+      </h1>
+      <div className="absolute rotate-12 rounded bg-red-700 px-2 text-sm">
+        Server Error!
+      </div>
+      <button className="mt-5">
+        <Link
+          className={
+            "group relative inline-block text-sm font-medium text-red-700 focus:outline-none focus:ring active:text-orange-500"
+          }
+          href={"/"}
+        >
+          <span className="absolute inset-0 translate-x-0.5 translate-y-0.5 bg-red-700 transition-transform group-hover:translate-x-0 group-hover:translate-y-0"></span>
+          <span className="relative block border border-current bg-sky-950 px-8 py-3">
+            Go Home
+          </span>
+        </Link>
+      </button>
+    </main>
+  );
+}
