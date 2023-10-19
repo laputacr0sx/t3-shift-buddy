bc061a97327f86cd73689dfc31687cccd650d42a
Veilics <laputafelix@gmail.com>
2023-10-19 12:59:41 +0800

    new tRPC procedure

--- src/server/api/routers/shiftController.ts
+++ src/server/api/routers/shiftController.ts
@@ -1,3 +1,4 @@
+import { TRPCError } from "@trpc/server/dist/error/TRPCError";
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
@@ -29,6 +30,31 @@ export const shiftControllerRouter = createTRPCRouter({
       const resultShiftArray = ctx.prisma.shifts.findMany({
         where: { dutyNumber: { in: input.shiftArray } },
       });
+
+      return resultShiftArray;
+    }),
+
+  getWholeWeekShiftWithPrefix: publicProcedure
+    .input(
+      z.object({
+        shiftArray: z.string().regex(sevenShiftRegex).array().length(7),
+      })
+    )
+    .query(async ({ input, ctx }) => {
+      const resultShiftArray = await ctx.prisma.shifts.findMany({
+        where: { dutyNumber: { in: input.shiftArray } },
+      });
+
+      const result = await ctx.prisma.weekPrefix.findMany({
+        orderBy: { updatedAt: "desc" },
+        take: 1,
+      });
+
+      if (!result)
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "result not found",
+        });
 
       return resultShiftArray;
     }),
