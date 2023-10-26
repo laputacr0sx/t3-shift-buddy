b41e8c6d9ab54c6f2be93698bbd0b2b22fcf5283
Veilics <laputafelix@gmail.com>
2023-10-26 16:06:24 +0800

    prisma.schema updated, _app cleared with nextAuth Session

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { ClerkProvider } from "@clerk/nextjs";
 
 import { type Session } from "next-auth";
-import { type AppProps } from "next/app";
+import { AppType, type AppProps } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
 import { Analytics } from "@vercel/analytics/react";
@@ -11,20 +11,30 @@ import { ThemeProvider } from "~/components/ui/theme-provider";
 import { type ReactElement, type ReactNode } from "react";
 import { type NextPage } from "next";
 
-export type NextPageWithLayout<P = unknown, IP = P> = NextPage<P, IP> & {
-  getLayout?: (page: ReactElement) => ReactNode;
+// export type NextPageWithLayout<P = unknown, IP = P> = NextPage<P, IP> & {
+//   getLayout?: (page: ReactElement) => ReactNode;
+// };
+
+// type AppPropsWithLayout<P> = AppProps<P> & {
+//   Component: NextPageWithLayout<P>;
+// };
+
+// const MyApp = ({
+//   Component,
+//   pageProps,
+// }: AppPropsWithLayout<{ session: Session | null }>) => {
+//   const getLayout = Component.getLayout ?? ((page) => page);
+
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: React.ReactElement) => React.ReactNode;
 };
 
-type AppPropsWithLayout<P> = AppProps<P> & {
-  Component: NextPageWithLayout<P>;
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
 };
 
-const MyApp = ({
-  Component,
-  pageProps,
-}: AppPropsWithLayout<{ session: Session | null }>) => {
+const MyApp = ({ Component, pageProps }: AppPropsWithLayout) => {
   const getLayout = Component.getLayout ?? ((page) => page);
-
   return (
     <ClerkProvider {...pageProps}>
       <ThemeProvider
