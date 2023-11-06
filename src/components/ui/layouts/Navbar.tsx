a86f9df1f4fcdb63647ba453048506a6943504b8
Veilics <laputafelix@gmail.com>
2023-11-07 01:17:59 +0800

    restyled navbar with caption

--- src/components/ui/layouts/Navbar.tsx
+++ src/components/ui/layouts/Navbar.tsx
@@ -14,7 +14,7 @@ function Navbar() {
   const { isSignedIn } = useUser();
 
   return (
-    <div className="fixed bottom-0 left-0 right-0 z-[9999] flex h-10 justify-between bg-navbar text-navbar-foreground">
+    <div className="fixed bottom-0 left-0 right-0 z-[9999] flex h-12 justify-between bg-navbar text-navbar-foreground">
       <div className="mx-2 self-center align-middle">
         {isSignedIn ? (
           <UserButton afterSignOutUrl="/" />
@@ -26,23 +26,39 @@ function Navbar() {
       </div>
       <ul className="flex flex-row justify-start gap-2 ">
         <li>
-          <Link href={"/"} className={"flex flex-row "}>
-            <Home className="m-2" />
+          <Link
+            href={"/"}
+            className={"flex flex-col items-center justify-center gap-0"}
+          >
+            <Home className="m-2" size={18} />
+            <p className="text-[10px]">首頁</p>
           </Link>
         </li>
         <li>
-          <Link href={"/allShifts"} className={"flex flex-row "}>
-            <Table className="m-2" />
+          <Link
+            href={"/allShifts"}
+            className={"flex flex-col items-center justify-center"}
+          >
+            <Table className="m-2" size={18} />
+            <p className="text-[10px]">更餡</p>
           </Link>
         </li>
         <li>
-          <Link href={"/exchange"} className={"flex flex-row "}>
-            <ArrowLeftRight className="m-2" />
+          <Link
+            href={"/exchange"}
+            className={"flex flex-col items-center justify-center"}
+          >
+            <ArrowLeftRight className="m-2" size={18} />
+            <p className="text-[10px]">調更</p>
           </Link>
         </li>
         <li>
-          <Link href={"/feature"} className={"flex flex-row "}>
-            <FlaskConical className="m-2" />
+          <Link
+            href={"/feature"}
+            className={"flex flex-col items-center justify-center"}
+          >
+            <FlaskConical className="m-2" size={18} />
+            <p className="text-[10px]">實驗</p>
           </Link>
         </li>
       </ul>
