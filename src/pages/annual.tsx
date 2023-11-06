f991a082d909b6c468725763de2c8c129af03a37
Veilics <laputafelix@gmail.com>
2023-11-07 01:11:48 +0800

    configuring annualLeave Calendar

--- src/pages/annual.tsx
+++ src/pages/annual.tsx
@@ -1,13 +1,53 @@
-import React, { type ReactElement } from "react";
+import React, { useState, type ReactElement } from "react";
 import { type NextPageWithLayout } from "./_app";
 import Layout from "~/components/ui/layouts/AppLayout";
 
 import { Calendar } from "~/components/ui/calendar";
+import moment from "moment";
+import { Button } from "~/components/ui/button";
+import { Minus, Plus, RotateCcw } from "lucide-react";
 
 const AnnualLeaves: NextPageWithLayout = () => {
+  const [year, setYear] = useState(moment().year());
+
   return (
     <div>
+      <h1 className="justify-center py-5 text-center text-4xl font-semibold text-foreground">
+        大假易
+      </h1>
+      {/* <div className="flex items-center justify-center pt-2 font-mono font-extrabold">
+        年份
+        <Button
+          onClick={() => {
+            setYear(() => {
+              return year - 1;
+            });
+          }}
+        >
+          <Minus size={20} />
+        </Button>
+        {year}
+        <Button
+          onClick={() => {
+            setYear(() => {
+              return year + 1;
+            });
+          }}
+        >
+          <Plus size={20} />
+        </Button>
+        <Button
+          onClick={() => {
+            setYear(() => {
+              return moment().year();
+            });
+          }}
+        >
+          <RotateCcw size={20} />
+        </Button>
+      </div> */}
       <Calendar
+        className=""
         weekStartsOn={1}
         showWeekNumber
         ISOWeek
@@ -18,9 +58,12 @@ const AnnualLeaves: NextPageWithLayout = () => {
           e.preventDefault();
           console.log({ weekNumber, dates, e });
         }}
-        onDayClick={(day, activeModifiers, e) => {
-          e.preventDefault();
-          console.log({ day, activeModifiers, e });
+        formatters={{
+          formatWeekNumber: (weekNumber) => (
+            <p className="flex items-center justify-center self-center text-center align-middle">
+              W{weekNumber}
+            </p>
+          ),
         }}
       />
     </div>
