106b1c3cccba6a466c562d36f3def40ffa00b151
Veilics <laputafelix@gmail.com>
2023-11-07 21:41:54 +0800

    configured annual.tsx with better selectedWeeks handling

--- src/pages/annual.tsx
+++ src/pages/annual.tsx
@@ -1,23 +1,43 @@
-import React, { useState, type ReactElement, useMemo, useEffect } from "react";
+import React, { useState, type ReactElement, useMemo } from "react";
+import { type WeekNumberClickEventHandler } from "react-day-picker";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
+
 import { type NextPageWithLayout } from "./_app";
 import Layout from "~/components/ui/layouts/AppLayout";
 
 import { Calendar } from "~/components/ui/calendar";
 import moment from "moment";
 import { Button } from "~/components/ui/button";
-import { Minus, Plus, RotateCcw } from "lucide-react";
-import { toast } from "~/components/ui/useToast";
 
 import { cn } from "~/lib/utils";
 import holidayJson from "~/utils/holidayHK";
 import fixtures from "~/utils/hkjcFixture";
 import { Card } from "~/components/ui/card";
 import { Separator } from "~/components/ui/separator";
-import WeekNumber from "../components/WeekNumber";
-import { WeekNumberClickEventHandler } from "react-day-picker";
+import { Label } from "~/components/ui/label";
+
+const FixtureLegend = () => {
+  return (
+    <Card className="absolute right-4 top-4 flex h-10 w-16 flex-col items-start justify-center border-0 px-2">
+      <section className="flex items-center justify-center gap-2 align-middle">
+        <Separator className="w-4  border-2 border-lime-500 dark:border-lime-300" />
+        <p className="text-[6px] text-lime-500 dark:text-lime-500">日馬</p>
+      </section>
+      <section className="flex items-center justify-center gap-2 align-middle">
+        <Separator className="w-4  border-2 border-indigo-500 dark:border-indigo-300 " />
+        <p className="text-[6px] text-indigo-500 dark:text-indigo-300">夜馬</p>
+      </section>
+      <section className="flex items-center justify-center gap-2 align-middle">
+        <Separator className="w-4  border-2 border-amber-500 dark:border-amber-300 " />
+        <p className="text-[6px] text-amber-500 dark:text-amber-300">黃昏馬</p>
+      </section>
+    </Card>
+  );
+};
 
 const AnnualLeaves: NextPageWithLayout = () => {
-  const [year, setYear] = useState(moment().year());
+  const [parent, enableAnimations] = useAutoAnimate(/* optional config */);
+
   const [selectedWeeks, setSelectedWeeks] = useState<string[]>([]);
   const [annualLimit, setAnnualLimit] = useState(3);
 
@@ -59,24 +79,7 @@ const AnnualLeaves: NextPageWithLayout = () => {
       <h1 className="justify-center py-5 text-center text-4xl font-semibold text-foreground">
         大假易
       </h1>
-      <Card className="absolute right-4 top-4 flex h-10 w-16 flex-col items-start justify-center border-0 px-2">
-        <section className="flex items-center justify-center gap-2 align-middle">
-          <Separator className="w-4  border-2 border-lime-500 dark:border-lime-300" />
-          <p className="text-[6px] text-lime-500 dark:text-lime-500">日馬</p>
-        </section>
-        <section className="flex items-center justify-center gap-2 align-middle">
-          <Separator className="w-4  border-2 border-indigo-500 dark:border-indigo-300 " />
-          <p className="text-[6px] text-indigo-500 dark:text-indigo-300">
-            夜馬
-          </p>
-        </section>
-        <section className="flex items-center justify-center gap-2 align-middle">
-          <Separator className="w-4  border-2 border-amber-500 dark:border-amber-300 " />
-          <p className="text-[6px] text-amber-500 dark:text-amber-300">
-            黃昏馬
-          </p>
-        </section>
-      </Card>
+      <FixtureLegend />
       <Calendar
         // fixedWeeks
         // disableNavigation
@@ -127,7 +130,6 @@ const AnnualLeaves: NextPageWithLayout = () => {
               </section>
             );
           },
-
           formatWeekNumber: (weekNumber) => (
             <p className="font-mono font-bold text-orange-900 dark:text-orange-300">
               W{weekNumber.toString().padStart(2, "0")}
@@ -136,22 +138,46 @@ const AnnualLeaves: NextPageWithLayout = () => {
         }}
       />
 
-      <p className="text-lg font-extrabold tracking-wider text-orange-800 dark:text-orange-400">
-        {selectedWeeks.map((weekNumber) => (
-          <span key={weekNumber} className="tracking-wider">
-            {weekNumber}
-          </span>
-        ))}
-      </p>
-      <Button
-        variant={"outline"}
-        onClick={() => {
-          localStorage.setItem("selectedWeeks", JSON.stringify(selectedWeeks));
-        }}
-        disabled
-      >
-        Save
-      </Button>
+      {selectedWeeks.length > 0 ? (
+        <p
+          className="flex gap-3 text-sm font-extrabold text-orange-800 dark:text-orange-400"
+          ref={parent}
+        >
+          已選週次為{" "}
+          {selectedWeeks.map((weekNumber) => (
+            <span key={weekNumber}>
+              W{weekNumber.toString().padStart(2, "0")}
+            </span>
+          ))}
+        </p>
+      ) : (
+        <p className="flex gap-3 text-lg font-extrabold text-amber-800 dark:text-amber-400">
+          您可選取最多 {annualLimit} 個週次
+        </p>
+      )}
+      <section className="flex flex-col justify-center gap-4">
+        <Button
+          variant={"outline"}
+          onClick={() => {
+            localStorage.setItem(
+              "selectedWeeks",
+              JSON.stringify(selectedWeeks)
+            );
+          }}
+          disabled
+        >
+          Save
+        </Button>
+        <Button
+          variant={"destructive"}
+          onClick={() => {
+            setSelectedWeeks([]);
+          }}
+          disabled={selectedWeeks.length === 0}
+        >
+          Reset
+        </Button>
+      </section>
     </div>
   );
 };
