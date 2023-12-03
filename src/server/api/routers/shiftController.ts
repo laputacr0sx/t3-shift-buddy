fd7a90a863c5779517b28c493705ccf5dfd49996
Veilics <laputafelix@gmail.com>
2023-12-03 22:39:14 +0800

    configuring upstash ratelimiter

--- src/server/api/routers/shiftController.ts
+++ src/server/api/routers/shiftController.ts
@@ -14,15 +14,35 @@ import {
   shiftNameRegex,
 } from "~/utils/regex";
 
+import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
+import { Redis } from "@upstash/redis"; // see below for cloudflare and fastly adapters
+
+// Create a new ratelimiter, that allows 3 requests per 1 minute.
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(3, "1 m"),
+  analytics: true,
+  /**
+   * Optional prefix for the keys used in redis. This is useful if you want to share a redis
+   * instance with other applications and want to avoid key collisions. The default prefix is
+   * "@upstash/ratelimit"
+   */
+  prefix: "@upstash/ratelimit",
+});
+
 export const shiftControllerRouter = createTRPCRouter({
-  getAllShifts: publicProcedure.query(({ ctx }) =>
-    ctx.prisma.shift.findMany({
+  getAllShifts: publicProcedure.query(async ({ ctx }) => {
+    const { success } = await ratelimit.limit("");
+
+    if (!success) throw new TRPCError({ code: "TOO_MANY_REQUESTS" });
+
+    return ctx.prisma.shift.findMany({
       where: { dutyNumber: { contains: "" } },
       orderBy: {
         dutyNumber: "asc",
       },
-    })
-  ),
+    });
+  }),
 
   getAllShiftsWithInfinite: publicProcedure
     .input(
