d1bad519d499b799343107c0aae26b211cb5d905
Felix <laputafelix@gmail.com>
2023-12-23 09:41:43 +0800

    Configure middleware

--- src/server/api/routers/userController.ts
+++ src/server/api/routers/userController.ts
@@ -1,6 +1,12 @@
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 import { prefixRegex } from "~/utils/regex";
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import {
+  createTRPCRouter,
+  protectedProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
 
-export const userControllerRouter = createTRPCRouter({});
+export const userControllerRouter = createTRPCRouter({
+  // createMetaData: protectedProcedure.input().mutation(),
+});
