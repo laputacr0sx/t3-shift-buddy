0b85b0426ef00c3adf6ad144f77bd653c7cf6956
Felix <laputafelix@gmail.com>
2024-04-06 22:27:24 +0800

    Edit weather display at index.tsx
    home page updated with new weather Info (High temp & Low temp)

--- src/components/WeatherIconDisplay.tsx
+++ src/components/WeatherIconDisplay.tsx
@@ -1,23 +1,30 @@
-import { convertWeatherIcons } from "~/utils/helper";
-import Image from "next/image";
-import { Weather } from "~/utils/customTypes";
+import { convertWeatherIcons } from '~/utils/helper';
+import Image from 'next/image';
+import type { Weather } from '~/utils/customTypes';
+import { Label } from './ui/label';
 
 type WeatherIconProps = {
-    weather: Weather | null
-}
+    weather: Weather | null;
+};
 export default function WeatherIconDisplay({ weather }: WeatherIconProps) {
-    if (!weather) return null
+    if (!weather) return null;
     const iconId = weather?.ForecastIcon.toString();
     const iconURI = convertWeatherIcons(iconId);
+    const hTemp = weather.forecastMaxtemp.value;
+    const lTemp = weather.forecastMintemp.value;
+
     return iconId ? (
-        <Image
-            src={`/image/weatherIcons/animated/${iconURI}.svg`}
-            alt={`${iconURI}`
-            }
-            width={30}
-            height={30}
-        />
-    ) : null
+        <>
+            <Image
+                src={`/image/weatherIcons/animated/${iconURI}.svg`}
+                alt={`${iconURI}`}
+                width={30}
+                height={30}
+            />
+            <Label className="text-xs">
+                <p className="text-red-700 dark:text-red-400">{hTemp}</p>
+                <p className="text-blue-700 dark:text-blue-400">{lTemp}</p>
+            </Label>
+        </>
+    ) : null;
 }
-
-
